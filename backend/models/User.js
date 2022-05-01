1b5041f8b4209763064eecb1bb954a2d809936fb
Guilherme Gevaerd <guilherme.gevaerd@tuntscorp.com>
2022-05-01 19:31:38 -0300

    feat(): add id to login

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,15 @@
 const db = require('../database');
 
 module.exports = class User {
-  constructor(name, roleName, isOp) {
+  constructor(id, name, roleName, isOp) {
+    this.id = id;
     this.name = name;
     this.roleName = roleName;
     this.isOp = isOp;
   }
 
   static async login(username, password) {
-    const [response] = await db.execute('SELECT u.name, r.id, r.name as role FROM users u JOIN credentials c ON c.user_id = u.id JOIN roles r ON u.role_id = r.id WHERE c.username = ? AND c.encrypted_password = ?', [username, password]);
-    return new User(response[0].name, response[0].role, response[0].id === 2);
+    const [response] = await db.execute('SELECT u.id, u.name, r.id, r.name as role FROM users u JOIN credentials c ON c.user_id = u.id JOIN roles r ON u.role_id = r.id WHERE c.username = ? AND c.encrypted_password = ?', [username, password]);
+    return new User(response[0].id, response[0].name, response[0].role, response[0].id === 2);
   }
 };
