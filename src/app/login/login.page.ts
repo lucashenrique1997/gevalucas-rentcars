92e8c3a35ad6094c2fc68514bbd05844e4b3e4c1
Lucas Vitorino <lucas.henrique@tuntscorp.com>
2022-05-01 20:01:31 -0300

    feat(*): finished

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import {BackendService} from "../services/backend.service";
-import {User} from "../models/User";
-import {Router} from "@angular/router";
-import {OverlayService} from "../../services/overlay.service";
+import {BackendService} from '../services/backend.service';
+import {User} from '../models/User';
+import {Router} from '@angular/router';
+import {OverlayService} from '../../services/overlay.service';
 
 
 @Component({
@@ -18,7 +18,7 @@ export class LoginPage implements OnInit {
     private formBuilder: FormBuilder,
     private backendService: BackendService,
     public router: Router,
-    private overlayService: OverlayService
+    private overlayService: OverlayService,
   ) {
   }
 
@@ -38,8 +38,12 @@ export class LoginPage implements OnInit {
     const loading = await this.overlayService.loading();
     try {
       this.backendService.login(this.form.value.user, this.form.value.password).subscribe(async (value: User) => {
-        console.log(value);
-        await this.router.navigateByUrl('/', {state: value});
+        console.log('value => ', value);
+        window.localStorage.setItem('isOp', value.isOp ? 'true' : 'false');
+        window.localStorage.setItem('userId', value.id.toString());
+        const url = value.isOp ? '/tabs/tab2' : '/tabs/tab1';
+        console.log('url === ', url);
+        await this.router.navigateByUrl(url, {state: value});
       });
     } catch (e) {
       await this.overlayService.toast({
@@ -48,7 +52,6 @@ export class LoginPage implements OnInit {
     } finally {
       await loading.dismiss();
     }
-
   }
 
 }
